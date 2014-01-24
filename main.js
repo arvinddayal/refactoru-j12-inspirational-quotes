5b2f68d432bb941f2591e472e91fcaf6e5f1af7d
arvindayal <arvinddayal@gmail.com>
2014-01-23 17:34:06 -0700

    working on form validation

--- main.js
+++ main.js
@@ -8,8 +8,13 @@ var load = function() {
 	$('#editor').on('click', toggleInput); 
 
 
+
+	var deleteButton = "<button type='button' class='delete-button'>Delete</button>";
+
+	var ratingSystem = "<input type='radio' name='group1' value'1 Star'>1 Star <input type='radio' name='group1' value'2 Star'>2 Star <input type='radio' name='group1' value'3 Star'>3 Star <input type='radio' name='group1' value'4 Star'>4 Star <input type='radio' name='group1' value'5 Star'>5 Star <br>";
+
 	var getNewQuote = function() {
-		$("#quote-list").append($("<li>" + $('#quote-input').val() + "<br />" + "<span class='author-item'>" + '&mdash; ' + $('#author-input').val() + "</span>" + "<button type='button' class='delete-button'>Delete</button>" + "</li>"));
+		$("#quote-list").append($("<li>" + $('#quote-input').val() + "<br />" + "<span class='author-item'>" + '&mdash; ' + $('#author-input').val() + "</span>" + "<br />" + ratingSystem + deleteButton + "</li>"));
 
 	};
 
@@ -21,7 +26,12 @@ var load = function() {
 
 	$('#quote-form').submit(function(event) {
 		event.preventDefault();
+		if (($('#quote-input') || $('#author-input')) === null) {
+			alert("You left a field blank");
+		}
+		else {
 		getNewQuote();
+		}
 	});
 
 
